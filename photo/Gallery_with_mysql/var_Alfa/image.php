2603fe0005f4b8e6a7c023f81c0ca6686d4aa775
Den787 <Den787@yandex.ru>
2018-11-29 17:42:37 +0400

    альфа версия

---
+++ photo/Gallery_with_mysql/var_Alfa/image.php
@@ -0,0 +1,21 @@
+<?php
+$filename = $_GET['pictureName'];
+
+$directoryBig = 'img/big/';
+?>
+<html>
+<head>
+    <title>Просмотр фотографии</title>
+    <meta http-equiv="imagetoolbar" content="no">
+</head>
+<body marginheight="0" marginwidth="0" rightmargin="0" bottommargin="0" leftmargin="0" topmargin="0" bgcolor="white">
+<img src="<? echo "{$directoryBig}.{$filename};" ?>" border="0">
+</body>
+</html>
+
+
+<!--
+и в основном коде другого файла, стоит вывод этого изображения
+PHPВыделить код <
+ echo '<img src="image.php?text='.$text.'&font='.$_POST['font'].'" alt="" title="" />' ; ?>
+-->
